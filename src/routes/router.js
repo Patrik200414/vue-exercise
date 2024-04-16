4d00b81566432e3815504d159b4ba5d57a355ad2
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-04-16 13:42:21 +0200

    Create dashboard route

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,11 +1,16 @@
 import {createWebHistory, createRouter} from 'vue-router'
 
 import Login from '../pages/Login.vue';
+import Dashboard from '../pages/Dashboard.vue';
 
 const routes = [
     {
         path: '/',
         component: Login
+    },
+    {
+        path: '/dashboard',
+        component: Dashboard
     }
 ]
 
