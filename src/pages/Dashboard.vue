b441ba1eea4a99a4a03a3e0075e75faa763e8a2b
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-04-16 13:42:59 +0200

    Implement getUser method

---
+++ src/pages/Dashboard.vue
@@ -0,0 +1,27 @@
+<template>
+    
+</template>
+
+<script>
+export default{
+    name: 'Dashboard',
+    data(){
+        return{
+            user: {}
+        }
+    },
+    mounted(){
+        this.getUser();
+    },
+    methods: {
+        getUser(){
+            const userData = JSON.parse(localStorage.getItem('USER_TOKEN'));
+            if(userData){
+                this.user = userData;
+            } else{
+                this.$router.push('/');
+            }
+        }
+    }
+}
+</script>
