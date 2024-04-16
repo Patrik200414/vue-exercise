b990c8cf0b7dd5c9b973b97ae2bf483b4c4b3b4a
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-04-16 14:09:24 +0200

    Create UserTable component

---
+++ src/components/TableRow.vue
@@ -0,0 +1,19 @@
+<template>
+    <tr>
+        <td>{{userData.email}}</td>
+        <td>{{userData.password}}</td>
+        <td>{{userData.token}}</td>
+    </tr>
+</template>
+
+<script>
+export default{
+    name: 'TableRow',
+    props: {
+        userData: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
---
+++ src/components/UserTable.vue
@@ -0,0 +1,29 @@
+<template>
+    <table class="table">
+        <thead>
+            <th>Email: </th>
+            <th>Password: </th>
+            <th>Token: </th>
+        </thead>
+        <tbody>
+            <TableRow :userData="userData"></TableRow>
+        </tbody>
+    </table>
+</template>
+
+<script>
+import TableRow from './TableRow.vue';
+
+export default{
+    name: 'UserTable',
+    props: {
+        userData: {
+            type: Object,
+            required: true
+        }
+    },
+    components: {
+        TableRow: TableRow,
+    }
+}
+</script>
